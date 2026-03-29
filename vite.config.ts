70ed4daba7090ccb0075f66cffa04b802338aff5
Pewgun <50993069+Pewgun@users.noreply.github.com>
2026-03-29 17:41:55 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'enchanting-presence-production-c5e5.up.railway.app'
+    ]
+  }
 })
