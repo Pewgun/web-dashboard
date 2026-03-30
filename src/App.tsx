0ce4e54190108e190b51284add0ae2a39aa975dd
Pewgun <50993069+Pewgun@users.noreply.github.com>
2026-03-30 21:30:41 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import SearchBar from './components/SearchBar';
 import './App.css';
 import './index.css'
 
-#import "tailwindcss";
+//import "tailwindcss";
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://0.0.0.0:3000';
 
