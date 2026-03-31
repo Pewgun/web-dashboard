685a657c63fc0fc202f8d284b31789d6b5ad0b18
Pewgun <50993069+Pewgun@users.noreply.github.com>
2026-03-31 19:30:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   css: {
-    lightningcss: false,
+    lightningcss: undefined as any
   },
   preview: {
     allowedHosts: [
