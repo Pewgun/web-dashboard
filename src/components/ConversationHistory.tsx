8a7bce67741c268641e3a0e5341a30dafdceecc4
Pewgun <50993069+Pewgun@users.noreply.github.com>
2026-04-01 13:32:38 +0300

    Update ConversationHistory.tsx

--- src/components/ConversationHistory.tsx
+++ src/components/ConversationHistory.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { formatDistanceToNow } from 'date-fns';
+import { format, isValid } from 'date-fns';
 import { enUS } from 'date-fns/locale';
 
 //export interface Conversation {
